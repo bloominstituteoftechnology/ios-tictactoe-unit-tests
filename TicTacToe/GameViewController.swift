519e6a512cc3403a055378e3713a3096cae00589
stdemicheli <stdemicheli@gmail.com>
2018-09-12 23:37:18 +0200

    Refactored GameViewController. Now uses Game as its logic.

--- TicTacToe/GameViewController.swift
+++ TicTacToe/GameViewController.swift
@@ -9,36 +9,17 @@
 import UIKit
 
 class GameViewController: UIViewController, BoardViewControllerDelegate {
-    
-    private enum GameState {
-        case active(GameBoard.Mark) // Active player
-        case cat
-        case won(GameBoard.Mark) // Winning player
-    }
-    
+        
     @IBAction func restartGame(_ sender: Any) {
-        board = GameBoard()
-        gameState = .active(.x)
+        game.restart()
     }
     
     // MARK: - BoardViewControllerDelegate
     
     func boardViewController(_ boardViewController: BoardViewController, markWasMadeAt coordinate: Coordinate) {
-        guard case let GameState.active(player) = gameState else {
-            NSLog("Game is over")
-            return
-        }
-        
         do {
-            try board.place(mark: player, on: coordinate)
-            if game(board: board, isWonBy: player) {
-                gameState = .won(player)
-            } else if board.isFull {
-                gameState = .cat
-            } else {
-                let newPlayer = player == .x ? GameBoard.Mark.o : GameBoard.Mark.x
-                gameState = .active(newPlayer)
-            }
+            try game.makeMark(at: coordinate)
+            updateViews()
         } catch {
             NSLog("Illegal move")
         }
@@ -49,13 +30,16 @@ class GameViewController: UIViewController, BoardViewControllerDelegate {
     private func updateViews() {
         guard isViewLoaded else { return }
         
-        switch gameState {
-        case let .active(player):
-            statusLabel.text = "Player \(player.stringValue)'s turn"
-        case .cat:
+        if game.board.isFull {
             statusLabel.text = "Cat's game!"
-        case let .won(player):
-            statusLabel.text = "Player \(player.stringValue) won!"
+        } else if game.gameIsOver && game.winningPlayer != nil {
+            if let winningPlayer = game.winningPlayer {
+                statusLabel.text = "Player \(winningPlayer.stringValue) won!"
+            }
+        } else {
+            if let activePlayer = game.activePlayer {
+                statusLabel.text = "Player \(activePlayer.stringValue)'s turn"
+            }
         }
     }
     
@@ -67,27 +51,22 @@ class GameViewController: UIViewController, BoardViewControllerDelegate {
         }
     }
     
+    private var game = Game() {
+        didSet {
+            boardViewController.board = game.board
+            updateViews()
+        }
+    }
+    
     private var boardViewController: BoardViewController! {
         willSet {
             boardViewController?.delegate = nil
         }
         didSet {
-            boardViewController?.board = board
+            boardViewController?.board = game.board
             boardViewController?.delegate = self
         }
     }
     
     @IBOutlet weak var statusLabel: UILabel!
-    
-    private var gameState = GameState.active(.x) {
-        didSet {
-            updateViews()
-        }
-    }
-    
-    private var board = GameBoard() {
-        didSet {
-            boardViewController.board = board
-        }
-    }
 }
