52b21fc5ab25345e5441deb885d533be8d31b340
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2018-10-17 17:49:05 -0500

    Game completed

--- TicTacToe/Game.swift
+++ TicTacToe/Game.swift
@@ -10,19 +10,44 @@ import Foundation
 
 struct Game {
     
-    private(set) var board: GameBoard
+    private(set) var board = GameBoard()
     
-    internal var activePlayer: GameBoard.Mark?
-    internal var winnigPlayer: GameBoard.Mark?
-    internal var gameIsOver: Bool
+    internal var activePlayer: GameBoard.Mark? = .x
+    internal var winnigPlayer: GameBoard.Mark? = nil
+    internal var gameIsOver: Bool = false
     
     mutating internal func restart() {
         board = GameBoard()
+        gameIsOver = false
+        winnigPlayer = nil
         activePlayer = .x
     }
     
     mutating internal func makeMark(at coordinate: Coordinate) throws {
-        try! board.place(mark: activePlayer!, on: coordinate)
+        guard let currentPlayer = activePlayer else { return }
+        
+        try! board.place(mark: currentPlayer, on: coordinate)
+        
+        if game(board: board, isWonBy: currentPlayer) {
+            winnigPlayer = currentPlayer
+            gameOver()
+        } else if board.isFull {
+            gameOver()
+        } else {
+            updatePlayer()
+        }
     }
     
+    mutating func gameOver() {
+        gameIsOver = true
+        activePlayer = nil
+    }
+    
+    mutating func updatePlayer() {
+        if activePlayer == .x {
+            activePlayer = .o
+        } else {
+            activePlayer = .x
+        }
+    }
 }
