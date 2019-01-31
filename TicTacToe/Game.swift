5c721c91b330f741ab5eb51c2a9d4588c778caf3
Ben Hakes <bhhakes@gmail.com>
2019-01-30 18:36:29 -0600

    refactor logic out of gameVC into game.sw (UIupdates still needed)

---
+++ TicTacToe/Game.swift
@@ -0,0 +1,52 @@
+//
+//  Game.swift
+//  TicTacToe
+//
+//  Created by Benjamin Hakes on 1/30/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    
+    mutating internal func restart() {
+        self.board = GameBoard()
+        activePlayer = .x
+        gameIsOver = false
+    }
+    
+    mutating func makeMark(at coordinate: Coordinate) throws{
+        
+        guard let player = activePlayer else {
+            NSLog("Game is over")
+            return
+        }
+        
+        do {
+            try board.place(mark: player, on: coordinate)
+            if game(board: board, isWonBy: player) {
+                winningPlayer = player
+                activePlayer = nil
+                gameIsOver = true
+            } else if board.isFull {
+                activePlayer = nil
+                gameIsOver = true
+            } else {
+                let newPlayer = player == .x ? GameBoard.Mark.o : GameBoard.Mark.x
+                activePlayer = newPlayer
+            }
+        } catch let illegal {
+            NSLog("Illegal move")
+            throw illegal
+        }
+    }
+    
+    private(set) var board: GameBoard = GameBoard()
+    
+    var activePlayer: GameBoard.Mark? = .x
+    var gameIsOver: Bool?
+    var winningPlayer: GameBoard.Mark?
+
+    
+}
---
+++ TicTacToeTests/GameTests.swift
@@ -0,0 +1,163 @@
+//
+//  GameTests.swift
+//  TicTacToeTests
+//
+//  Created by Benjamin Hakes on 1/30/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import XCTest
+@testable import TicTacToe
+
+class GameTests: XCTestCase {
+
+    func testGameIsOver() {
+        
+        var newGame = Game()
+        /*
+         x o x
+         x o x
+         o x o
+         */
+        try! newGame.makeMark(at: (2, 1)) // x
+        try! newGame.makeMark(at: (1, 1)) // o
+        try! newGame.makeMark(at: (2, 0)) // x
+        try! newGame.makeMark(at: (2, 2)) // o
+        try! newGame.makeMark(at: (0, 0)) // x
+        try! newGame.makeMark(at: (1, 0)) // o
+        try! newGame.makeMark(at: (1, 2)) // x
+        try! newGame.makeMark(at: (0, 2)) // o
+        try! newGame.makeMark(at: (0, 1)) // x
+        
+        guard let gameIsOver = newGame.gameIsOver else {
+            XCTAssertTrue(false)
+            return
+        }
+        XCTAssertTrue(gameIsOver)
+        
+    }
+    
+    func testNSLogErrorOnDuplicateMark() {
+        
+        var newGame = Game()
+        /*
+         - - -
+         - - x
+         - - -
+         */
+        try! newGame.makeMark(at: (2, 1)) // x
+        XCTAssertThrowsError(try newGame.makeMark(at: (2, 1))) // o
+    }
+    
+    func testActivePlayer() {
+        
+        var newGame = Game()
+        /*
+         x - -
+         - - -
+         - - -
+         */
+        try! newGame.makeMark(at: (0, 0)) // x
+        
+        guard let activePlayer = newGame.activePlayer else {
+            XCTAssertTrue(false)
+            return
+        }
+        XCTAssertTrue(activePlayer == GameBoard.Mark.o)
+        
+    }
+    
+    func testActivePlayer2() {
+        
+        var newGame = Game()
+        /*
+         x - -
+         - - -
+         - - -
+         */
+        try! newGame.makeMark(at: (0, 0)) // x
+        try! newGame.makeMark(at: (0, 1)) // o
+        
+        guard let activePlayer = newGame.activePlayer else {
+            XCTAssertTrue(false)
+            return
+        }
+        XCTAssertTrue(activePlayer == GameBoard.Mark.x)
+        XCTAssertFalse(activePlayer == GameBoard.Mark.o)
+        
+    }
+    
+    func testWinningPlayer() {
+        
+        var newGame = Game()
+        /*
+         x o x
+         x o x
+         - o o
+         */
+        try! newGame.makeMark(at: (2, 1)) // x
+        try! newGame.makeMark(at: (1, 1)) // o
+        try! newGame.makeMark(at: (2, 0)) // x
+        try! newGame.makeMark(at: (2, 2)) // o
+        try! newGame.makeMark(at: (0, 0)) // x
+        try! newGame.makeMark(at: (1, 0)) // o
+        try! newGame.makeMark(at: (0, 1)) // x
+        try! newGame.makeMark(at: (1, 2)) // o
+        
+        guard let winningPlayer = newGame.winningPlayer else {
+            XCTAssertTrue(false)
+            return
+        }
+        XCTAssertTrue(winningPlayer == GameBoard.Mark.o)
+        
+    }
+    
+    func gameHasRestarted() {
+        
+        var newGame = Game()
+        /*
+         x o x
+         x o x
+         - o o
+         */
+        try! newGame.makeMark(at: (2, 1)) // x
+        try! newGame.makeMark(at: (1, 1)) // o
+        try! newGame.makeMark(at: (2, 0)) // x
+        try! newGame.makeMark(at: (2, 2)) // o
+        try! newGame.makeMark(at: (0, 0)) // x
+        try! newGame.makeMark(at: (1, 0)) // o
+        try! newGame.makeMark(at: (0, 1)) // x
+        try! newGame.makeMark(at: (1, 2)) // o
+        
+        newGame.restart()
+        /*
+         - - -
+         - - -
+         - - -
+         */
+        for x in 0..<3 {
+            for y in 0..<3 {
+                XCTAssertNil(newGame.board[(x, y)])
+            }
+        }
+        
+    }
+    
+    func boardIsEmpty() {
+        
+        let newGame = Game()
+        /*
+         - - -
+         - - -
+         - - -
+         */
+        
+        for x in 0..<3 {
+            for y in 0..<3 {
+                XCTAssertNil(newGame.board[(x, y)])
+            }
+        }
+        
+    }
+
+}
