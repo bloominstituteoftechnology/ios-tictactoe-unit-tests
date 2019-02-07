b53b5b5223b3bd0478f29c7c968611e764d1ed6f
Audrey Welch <audreywelch@Audreys-MacBook-Pro.local>
2019-02-07 11:06:41 -0600

    Began to build unit tests

--- TicTacToeTests/GameTests.swift
+++ TicTacToeTests/GameTests.swift
@@ -4,20 +4,32 @@ import XCTest
 
 class GameTests: XCTestCase {
 
-    override func setUp() {
-        var newGame = Game()
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testGameBoardEmpty() {
+    //var testGame: Game!
     
+    override func setUp() {
+        var testGame = Game()
+    }
+
+    func testCreateGame() {
+        var testGame = Game()
+        XCTAssertNotNil(testGame, "There is no valid game")
+    }
+    
+    func testGameBoardEmpty() {
+        let board = GameBoard()
+        
+        for x in 0..<3 {
+            for y in 0..<3 {
+                XCTAssertNil(board(x, y))
+            }
+        }
     }
 
     func testRestart() {
+       let board = GameBoard()
         
+        XCTAssertTrue(testGame.activePlayer = .x)
+        XCTAssert(testGame.gameIsOver = false)
     }
     
     
