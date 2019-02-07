5f782bff20765c0cbda7cddbe3d71db0095b63a3
IvanCaldwell <grumpy.caldwell@gmail.com>
2019-02-07 00:24:41 -0800

    Added GameTests.swift

--- TicTacToe/Game.swift
+++ TicTacToe/Game.swift
@@ -9,6 +9,23 @@
 import Foundation
 
 struct Game {
+
+    // board : Externally read-only property for the game board. Game itself
+    // modifies this as the game progresses.
+    private(set) var board: GameBoard = GameBoard()
+    
+    // activePlayer : The currently active player, either .x or .o . That is, the
+    // player whose turn it is. nil if the game is over.
+    internal var activePlayer: GameBoard.Mark? = .x
+    
+    // gameIsOver : true if the game is over (either won or a cat's game), false
+    // if the game is still running.
+    internal var gameIsOver: Bool
+    
+    // winningPlayer : The player that won the game, either .x or .o . nil if the
+    // game is still running, or it's a cat's game (no one won).
+    internal var winningPlayer: GameBoard.Mark?
+    
     
     // restart() : Restarts the game to a fresh state with an empty board,
     // and player X starting.
@@ -16,6 +33,7 @@ struct Game {
         board = GameBoard()
         activePlayer = .x
         gameIsOver = false
+        winningPlayer = nil
     }
     
     // makeMark(at:) : adds a mark for the currently active player at the given
@@ -44,19 +62,5 @@ struct Game {
         }
     }
     
-    // board : Externally read-only property for the game board. Game itself
-    // modifies this as the game progresses.
-    private(set) var board: GameBoard = GameBoard()
-   
-    // activePlayer : The currently active player, either .x or .o . That is, the
-    // player whose turn it is. nil if the game is over.
-    internal var activePlayer: GameBoard.Mark? = .x
-    
-    // gameIsOver : true if the game is over (either won or a cat's game), false
-    // if the game is still running.
-    internal var gameIsOver: Bool
-    
-    // winningPlayer : The player that won the game, either .x or .o . nil if the
-    // game is still running, or it's a cat's game (no one won).
-    internal var winningPlayer: GameBoard.Mark?
+
 }
