e581defb4b03464c67792d3d60bb53c54e40289e
Moses Robinson <mosesjrobinson@icloud.com>
2019-02-27 16:09:58 -0800

    all places are marked and no winners are found test passed

--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -108,8 +108,25 @@ class GameAITests: XCTestCase {
     }
     
     func testIncompleteGame() {
+        
     }
 
     func testCatsGame() {
+        /*
+         x o o
+         o x x
+         o x o
+         */
+        try! board.place(mark: .x, on: (0, 0))
+        try! board.place(mark: .o, on: (0, 1))
+        try! board.place(mark: .o, on: (0, 2))
+        try! board.place(mark: .o, on: (1, 0))
+        try! board.place(mark: .x, on: (1, 1))
+        try! board.place(mark: .x, on: (1, 2))
+        try! board.place(mark: .o, on: (2, 0))
+        try! board.place(mark: .x, on: (2, 1))
+        try! board.place(mark: .o, on: (2, 2))
+        XCTAssertFalse(game(board: board, isWonBy: .x))
+        XCTAssertFalse(game(board: board, isWonBy: .o))
     }
 }
