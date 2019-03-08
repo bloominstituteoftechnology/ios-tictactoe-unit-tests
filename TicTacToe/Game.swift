776396f9c8741c7a4dbc1832629b8d15d5f081c1
Paul Yi <yipaulc@gmail.com>
2019-03-08 01:09:15 -0800

    Add tests for Game

--- TicTacToe/Game.swift
+++ TicTacToe/Game.swift
@@ -10,24 +10,41 @@ import Foundation
 
 struct Game {
     
-    mutating internal func restart() {
-        board = GameBoard()
-        activePlayer = .x
-        gameIsOver = false
-        winningPlayer = nil
-    }
-    mutating internal func makeMark(at coordinate: Coordinate) throws {
-        guard let currentlyActivePlayer = activePlayer else { return }
-        try board.place(mark: currentlyActivePlayer, on: coordinate)
-        if board.isFull || winningPlayer != nil {
-            gameIsOver = true
-        }
-    }
-    
     // MARK: - Properties
+
+    private(set) var board = GameBoard()
     
-    private(set) var board: GameBoard
     internal var activePlayer: GameBoard.Mark? = .x
     internal var gameIsOver: Bool = false
     internal var winningPlayer: GameBoard.Mark? = nil
+    
+    mutating internal func restart() {
+        board = GameBoard()
+        gameIsOver = false
+        winningPlayer = nil
+        activePlayer = .x
+    }
+    
+    mutating internal func makeMark(at coordinate: Coordinate) throws {
+        guard let currentlyActivePlayer = activePlayer else { return }
+        
+        try board.place(mark: currentlyActivePlayer, on: coordinate)
+        
+        if game(board: board, isWonBy: currentlyActivePlayer) {
+            gameIsOver = true
+            winningPlayer = currentlyActivePlayer
+            activePlayer = nil
+        } else if board.isFull {
+            gameIsOver = true
+            winningPlayer = nil
+            activePlayer = nil
+        } else {
+            if activePlayer == .x {
+                activePlayer = .o
+            } else {
+                activePlayer = .x
+            }
+        }
+
+    }
 }
--- TicTacToeTests/GameTests.swift
+++ TicTacToeTests/GameTests.swift
@@ -7,3 +7,99 @@
 //
 
 import XCTest
+@testable import TicTacToe
+
+class GameTests: XCTestCase {
+    
+    func testXWins() throws {
+        var game = Game()
+        
+        try game.makeMark(at: (0, 0))  // x
+        try game.makeMark(at: (2, 0))  // o
+        try game.makeMark(at: (2, 2))  // x
+        try game.makeMark(at: (1, 2))  // o
+        try game.makeMark(at: (1, 1))  // x
+        
+        XCTAssertTrue(game.gameIsOver)
+        XCTAssertNil(game.activePlayer)
+        XCTAssertEqual(game.winningPlayer, .x)
+    }
+    
+    func testOWins() throws {
+        var game = Game()
+        
+        try game.makeMark(at: (1, 0))  // x
+        try game.makeMark(at: (1, 1))  // o
+        try game.makeMark(at: (0, 0))  // x
+        try game.makeMark(at: (2, 0))  // o
+        try game.makeMark(at: (1, 2))  // x
+        try game.makeMark(at: (0, 2))  // o
+        
+        XCTAssertTrue(game.gameIsOver)
+        XCTAssertNil(game.activePlayer)
+        XCTAssertEqual(game.winningPlayer, .o)
+    }
+    
+    func testCatsGame() throws {
+        var game = Game()
+        
+        try game.makeMark(at: (0, 0))  // x
+        try game.makeMark(at: (2, 2))  // o
+        try game.makeMark(at: (1, 1))  // x
+        try game.makeMark(at: (0, 1))  // o
+        try game.makeMark(at: (2, 1))  // x
+        try game.makeMark(at: (1, 0))  // o
+        try game.makeMark(at: (1, 2))  // x
+        try game.makeMark(at: (2, 0))  // o
+        try game.makeMark(at: (0, 2))  // x
+        
+        XCTAssertTrue(game.gameIsOver)
+        XCTAssertNil(game.activePlayer)
+        XCTAssertNil(game.winningPlayer)
+    }
+    
+    
+    func testInProgressGame() {
+        var game = Game()
+        
+        try! game.makeMark(at: (1, 0))  // x
+        try! game.makeMark(at: (1, 1))  // o
+        try! game.makeMark(at: (0, 0))  // x
+        
+        XCTAssertFalse(game.gameIsOver)
+        XCTAssertEqual(game.activePlayer, .o)
+        XCTAssertNil(game.winningPlayer)
+    }
+    
+    func testRestart() throws {
+        var game = Game()
+        
+        try game.makeMark(at: (0, 0))
+        
+        game.restart()
+        
+        XCTAssertFalse(game.gameIsOver)
+        XCTAssertEqual(game.activePlayer, .x)
+        XCTAssertNil(game.winningPlayer)
+        
+        try game.makeMark(at: (0, 0))  // x    
+        try game.makeMark(at: (2, 0))  // o
+        try game.makeMark(at: (2, 2))  // x
+        try game.makeMark(at: (1, 2))  // o
+        try game.makeMark(at: (1, 1))  // x
+        
+        XCTAssertTrue(game.gameIsOver)
+        XCTAssertNil(game.activePlayer)
+        XCTAssertEqual(game.winningPlayer, .x)
+        
+        game.restart()
+        
+        XCTAssertFalse(game.gameIsOver)
+        XCTAssertEqual(game.activePlayer, .x)
+        XCTAssertNil(game.winningPlayer)
+        
+    }
+    
+}
+
+
