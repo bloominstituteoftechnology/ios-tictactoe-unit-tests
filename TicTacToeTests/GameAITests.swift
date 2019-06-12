3a1ebab831b2f50887a530cf0d85b978a403ee82
Michael Redig <mredig@gmail.com>
2019-06-12 14:50:19 -0500

    finished game tests

--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -119,8 +119,44 @@ class GameAITests: XCTestCase {
     }
     
     func testIncompleteGame() {
+		var board = GameBoard()
+		/*
+		x - o
+		- o -
+		- x o
+		*/
+
+		try! board.place(mark: .o, on: (2, 0))
+		try! board.place(mark: .x, on: (0, 0))
+		try! board.place(mark: .o, on: (1, 1))
+		try! board.place(mark: .x, on: (1, 2))
+		try! board.place(mark: .o, on: (2, 2))
+
+		XCTAssertFalse(game(board: board, isWonBy: .o))
+		XCTAssertFalse(game(board: board, isWonBy: .x))
+
     }
 
     func testCatsGame() {
-    }
+		var board = GameBoard()
+		/*
+		x o o
+		o o x
+		x x o
+		*/
+
+		try! board.place(mark: .x, on: (0, 0))
+		try! board.place(mark: .o, on: (1, 0))
+		try! board.place(mark: .o, on: (2, 0))
+		try! board.place(mark: .o, on: (0, 1))
+		try! board.place(mark: .o, on: (1, 1))
+		try! board.place(mark: .x, on: (2, 1))
+		try! board.place(mark: .x, on: (0, 2))
+		try! board.place(mark: .x, on: (1, 2))
+		try! board.place(mark: .o, on: (2, 2))
+
+		XCTAssertFalse(game(board: board, isWonBy: .o))
+		XCTAssertFalse(game(board: board, isWonBy: .x))
+		XCTAssertTrue(board.isFull)
+	}
 }
