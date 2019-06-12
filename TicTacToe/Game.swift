bbd64139139e6b0d87511ada673e129d8a9a9637
Christopher Aronson <christopheraronson@icloud.com>
2019-06-12 13:29:41 -0700

    Added test units for several win and lose cases

---
+++ TicTacToe/Game.swift
@@ -0,0 +1,22 @@
+//
+//  Game.swift
+//  TicTacToe
+//
+//  Created by Christopher Aronson on 6/12/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    
+    private(set) var board: GameBoard
+    
+    internal var  activePlayer: GameBoard.Mark?
+    internal var gameIsOver: Bool
+    internal var winningPlayer: GameBoard.Mark?
+    
+    mutating internal func restart() {}
+    
+    mutating internal func makeMark(at coordinate: Coordinate)
+}
