024fc74e9969aae28f7acb2daa8b90ef70668d70
Hayden Hastings <haydenhastings32@gmail.com>
2019-06-19 17:51:54 -0600

    Added code for Game.swift Added file GameTests

--- TicTacToe/Game.swift
+++ TicTacToe/Game.swift
@@ -10,10 +10,34 @@ import Foundation
 
 struct Game {
     mutating internal func restart() {
-        
+        board = GameBoard()
+        activePlayer = .x
+        gameIsOver = false
     }
     mutating internal func makeMark(at coordinate: Coordinate) throws {
+        guard let player = activePlayer else {
+            NSLog("Game is Over")
+            return
+        }
         
+        do {
+            try board.place(mark: player, on: coordinate)
+            if game(board: board, isWonBy: player) {
+                
+                gameOver()
+                winningPlayer = player
+            } else {
+                let newPlayer = player == .x ? GameBoard.Mark.o : GameBoard.Mark.x
+                activePlayer = newPlayer
+            }
+        } catch {
+            NSLog("Not a move.")
+        }
+    }
+    
+    mutating func gameOver() {
+        activePlayer = nil
+        gameIsOver = true
     }
     
     private(set) var board: GameBoard
