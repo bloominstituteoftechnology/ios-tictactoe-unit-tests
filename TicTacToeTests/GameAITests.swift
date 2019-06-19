f5ff257ee846842773121bb68b5154b78b6f3480
Kobe McKee <kobe.mckee@gmail.com>
2019-06-19 14:54:14 -0600

    Added last test cases

--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -109,8 +109,53 @@ class GameAITests: XCTestCase {
     }
     
     func testIncompleteGame() {
+        
+        var board = GameBoard()
+        var nilCounter = 0
+        
+        try! board.place(mark: .x, on: (0, 0))
+        try! board.place(mark: .x, on: (2, 1))
+        try! board.place(mark: .x, on: (0, 2))
+        try! board.place(mark: .x, on: (1, 2))
+        try! board.place(mark: .o, on: (1, 0))
+        try! board.place(mark: .o, on: (0, 1))
+        try! board.place(mark: .o, on: (1, 1))
+        try! board.place(mark: .o, on: (2, 2))
+        
+        for x in 0 ..< 3 {
+            for y in 0 ..< 3 {
+                if board[(x, y)] == nil {
+                    nilCounter += 1
+                }
+            }
+        }
+        
+        XCTAssert(nilCounter != 0)
+        XCTAssertFalse(game(board: board, isWonBy: .o))
+        XCTAssertFalse(game(board: board, isWonBy: .x))
     }
 
     func testCatsGame() {
+        
+        var board = GameBoard()
+        
+        try! board.place(mark: .x, on: (0, 0))
+        try! board.place(mark: .x, on: (2, 1))
+        try! board.place(mark: .x, on: (0, 2))
+        try! board.place(mark: .x, on: (1, 2))
+        try! board.place(mark: .x, on: (2, 0))
+        try! board.place(mark: .o, on: (1, 0))
+        try! board.place(mark: .o, on: (0, 1))
+        try! board.place(mark: .o, on: (1, 1))
+        try! board.place(mark: .o, on: (2, 2))
+        
+        for x in 0 ..< 3 {
+            for y in 0 ..< 3 {
+                XCTAssertNotNil(board[(x, y)])
+            }
+        }
+        
+        XCTAssertFalse(game(board: board, isWonBy: .o))
+        XCTAssertFalse(game(board: board, isWonBy: .x))
     }
 }
