e11298853809f68408145d64135dfae13c014fcc
Thomas Cacciatore <tcacciatore597@gmail.com>
2019-06-26 16:06:58 -0400

    updated testCatsGame func.

--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -127,12 +127,22 @@ class GameAITests: XCTestCase {
     }
 
     func testCatsGame() {
+        var board = GameBoard()
         /*
          x x o
          o o x
          x o x
         */
-        
-        
+        try! board.place(mark: .x, on: (0, 2))
+        try! board.place(mark: .o, on: (2, 0))
+        try! board.place(mark: .x, on: (0, 0))
+        try! board.place(mark: .o, on: (0, 1))
+        try! board.place(mark: .x, on: (1, 0))
+        try! board.place(mark: .o, on: (1, 1))
+        try! board.place(mark: .x, on: (2, 1))
+        try! board.place(mark: .o, on: (1, 2))
+        try! board.place(mark: .x, on: (2, 2))
+        XCTAssertFalse(game(board: board, isWonBy: .x) && board.isFull)
+        XCTAssertFalse(game(board: board, isWonBy: .o) && board.isFull)
     }
 }
