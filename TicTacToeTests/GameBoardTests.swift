08f6ff5dccf41e56d2bbd732c0c67e2a3e8ac62f
bradleyyin <bradleyyin@gmail.com>
2019-09-11 09:43:31 -0700

    added an unit test target

---
+++ TicTacToeTests/GameBoardTests.swift
@@ -0,0 +1,19 @@
+//
+//  GameBoardTests.swift
+//  TicTacToeTests
+//
+//  Created by Bradley Yin on 9/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class GameBoardTests:XCTestCase {
+    
+    //unit test always start with test
+    func testGameBoard() {
+        XCTFail()
+    }
+    
+}
