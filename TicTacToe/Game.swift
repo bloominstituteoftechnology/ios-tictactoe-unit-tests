814d349dddb704e4e1c289638429e80c39a6001e
Jordan Christensen <jordan.c4922@gmail.com>
2019-10-10 04:47:21 +0800

    Add Game.swift file

---
+++ TicTacToe/Game.swift
@@ -0,0 +1,23 @@
+//
+//  Game.swift
+//  TicTacToe
+//
+//  Created by Jordan Christensen on 10/10/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    private(set) var board: GameBoard
+    internal var activePlayer: GameBoard.Mark?
+    internal var gameIsOver: Bool
+    internal var winningPlayer: GameBoard.Mark?
+
+    mutating internal func restart() {
+        
+    }
+    mutating internal func makeMark(at coordinate: Coordinate) throws {
+        
+    }
+}
