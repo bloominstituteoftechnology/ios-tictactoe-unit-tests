015a24fc2b04ff3b8e049c6498a16b0bce11ec6c
Fabiola <saga.fabiola@gmail.com>
2019-11-19 14:31:41 -0600

    Added test for horizontal1: test succeeded

--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -50,6 +50,14 @@ class GameAITests: XCTestCase {
          x x x
          o - -
          */
+        try! board.place(mark: .o, on: (1, 0))
+        try! board.place(mark: .x, on: (0, 1))
+        try! board.place(mark: .x, on: (1, 1))
+        try! board.place(mark: .x, on: (2, 1))
+        try! board.place(mark: .o, on: (0, 2))
+        XCTAssertTrue(game(board: board, isWonBy: .x))
+        XCTAssertFalse(game(board: board, isWonBy: .o))
+        
     }
     
     func testWinCheckingHorizontal2() {
