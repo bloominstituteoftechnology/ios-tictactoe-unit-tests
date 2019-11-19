c1f917ec347eb8c765d0ede2a1e35cc21f56e366
John Kouris <jdkouris@gmail.com>
2019-11-18 19:39:37 -0800

    Added testWinCheckingVertical2 test

--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -29,4 +29,22 @@ class GameAITests: XCTestCase {
         XCTAssertFalse(game(board: board, isWonBy: .o))
     }
     
+    func testWinCheckingVertical2() {
+        /*
+         o x -
+         o x -
+         - x -
+         */
+        var board = GameBoard()
+        
+        try! board.place(mark: .x, on: (1,0))
+        try! board.place(mark: .o, on: (0,0))
+        try! board.place(mark: .x, on: (1,1))
+        try! board.place(mark: .o, on: (0,1))
+        try! board.place(mark: .x, on: (1,2))
+        
+        XCTAssertTrue(game(board: board, isWonBy: .x))
+        XCTAssertFalse(game(board: board, isWonBy: .o))
+    }
+    
 }
