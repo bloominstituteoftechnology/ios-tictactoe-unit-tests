65abd8f1faf6f239b4998a258183bd8a15529354
Vici Shaweddy <vshaweddy@gmail.com>
2019-11-19 21:45:15 -0500

    testing the incomplete and cat's

---
+++ TicTacToe/Game.swift
@@ -0,0 +1,24 @@
+//
+//  Game.swift
+//  TicTacToe
+//
+//  Created by Vici Shaweddy on 11/19/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+//struct Game {
+//    mutating internal func restart() {
+//        board = GameBoard()
+//        gameState = .active(.x)
+//    }
+//    
+//    mutating internal func makeMark(at coordinate: Coordinate) throws
+//    
+//    private(set) var board: GameBoard
+//    
+//    internal var activePlayer: GameBoard.Mark?
+//    internal var gameIsOver: Bool
+//    internal var winningPlayer: GameBoard.Mark?
+//}
--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -108,8 +108,37 @@ class GameAITests: XCTestCase {
     }
     
     func testIncompleteGame() {
+        var board = GameBoard()
+        /*
+         x - -
+         - o -
+         o x -
+         */
+        try! board.place(mark: .x, on: (0, 0))
+        try! board.place(mark: .x, on: (1, 2))
+        try! board.place(mark: .o, on: (0, 2))
+        try! board.place(mark: .o, on: (1, 1))
+        XCTAssertFalse(game(board: board, isWonBy: .o))
+        XCTAssertFalse(game(board: board, isWonBy: .x))
     }
 
     func testCatsGame() {
+        var board = GameBoard()
+        /*
+         x x o
+         o o x
+         x x o
+         */
+        try! board.place(mark: .x, on: (0, 0))
+        try! board.place(mark: .x, on: (1, 0))
+        try! board.place(mark: .x, on: (2, 1))
+        try! board.place(mark: .x, on: (0, 2))
+        try! board.place(mark: .x, on: (1, 2))
+        try! board.place(mark: .o, on: (1, 1))
+        try! board.place(mark: .o, on: (2, 0))
+        try! board.place(mark: .o, on: (0, 1))
+        try! board.place(mark: .o, on: (2, 2))
+        XCTAssertFalse(game(board: board, isWonBy: .o))
+        XCTAssertFalse(game(board: board, isWonBy: .x))
     }
 }
