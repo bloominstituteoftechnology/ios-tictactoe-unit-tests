de986f2e6110c009e72c3936f1fa9c414ba5d20a
Fabiola <saga.fabiola@gmail.com>
2019-11-19 20:41:56 -0600

    Added test for CatsGame: test succeeded.

--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -118,5 +118,25 @@ class GameAITests: XCTestCase {
     }
 
     func testCatsGame() {
+        var board = GameBoard()
+        /*
+         x x o
+         o x x
+         x o o
+         */
+        
+        try! board.place(mark: .x, on: (0, 0))
+        try! board.place(mark: .x, on: (1, 0))
+        try! board.place(mark: .o, on: (2, 0))
+        try! board.place(mark: .o, on: (0, 1))
+        try! board.place(mark: .x, on: (1, 1))
+        try! board.place(mark: .x, on: (2, 1))
+        try! board.place(mark: .x, on: (0, 2))
+        try! board.place(mark: .o, on: (1, 2))
+        try! board.place(mark: .o, on: (2, 2))
+        XCTAssertFalse(game(board: board, isWonBy: .x))
+        XCTAssertFalse(game(board: board, isWonBy: .o))
+        XCTAssertTrue(board.isFull)
+        
     }
 }
