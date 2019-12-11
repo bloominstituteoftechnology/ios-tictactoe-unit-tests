ac079c740ac0585b2cb8cf5af187f7c7df66d4ef
Jon Bash <jonbash@protonmail.com>
2019-12-11 14:56:57 -0800

    rework gameVC logic for game struct

--- TicTacToe/GameViewController.swift
+++ TicTacToe/GameViewController.swift
@@ -9,13 +9,6 @@
 import UIKit
 
 class GameViewController: UIViewController, BoardViewControllerDelegate {
-    
-    private enum GameState {
-        case active(GameBoard.Mark) // Active player
-        case cat
-        case won(GameBoard.Mark) // Winning player
-    }
-    
     // MARK: - Properties
     
     private var boardViewController: BoardViewController! {
@@ -23,73 +16,57 @@ class GameViewController: UIViewController, BoardViewControllerDelegate {
             boardViewController?.delegate = nil
         }
         didSet {
-            boardViewController?.board = board
+            boardViewController?.board = game.board
             boardViewController?.delegate = self
-        }
-    }
-    
-    private var gameState = GameState.active(.x) {
-        didSet {
-            updateViews()
-        }
-    }
-    
-    private var board = GameBoard() {
-        didSet {
-            boardViewController.board = board
         }
     }
     
     private var game = Game() {
         didSet {
-            
+            updateViews()
         }
     }
     
-    // MARK: - Outlets/Actions
-    
     @IBOutlet weak var statusLabel: UILabel!
     
+    // MARK: - Methods
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateViews()
+    }
+    
     @IBAction func restartGame(_ sender: Any) {
-        board = GameBoard()
-        gameState = .active(.x)
+        game.restart()
+        updateViews()
+    }
+    
+    private func updateViews() {
+        guard isViewLoaded else { return }
+        
+        boardViewController.board = game.board
+        
+        if let player = game.activePlayer {
+            statusLabel.text = "Player \(player.stringValue)'s turn"
+        } else if let player = game.winningPlayer {
+            statusLabel.text = "Player \(player.stringValue) won!"
+        } else if game.gameIsOver {
+            statusLabel.text = "Cat's game!"
+        }
     }
     
     // MARK: - BoardViewControllerDelegate
     
     func boardViewController(_ boardViewController: BoardViewController, markWasMadeAt coordinate: Coordinate) {
-        guard case let GameState.active(player) = gameState else {
-            NSLog("Game is over")
-            return
-        }
-        
         do {
-            try board.place(mark: player, on: coordinate)
-            if game(board: board, isWonBy: player) {
-                gameState = .won(player)
-            } else if board.isFull {
-                gameState = .cat
-            } else {
-                let newPlayer = player == .x ? GameBoard.Mark.o : GameBoard.Mark.x
-                gameState = .active(newPlayer)
-            }
+            try game.makeMark(at: coordinate)
+            updateViews()
+        } catch GameBoardError.gameIsOver {
+            NSLog("Game is over")
+        } catch GameBoardError.invalidSquare {
+            NSLog("Invalid move")
         } catch {
-            NSLog("Illegal move")
-        }
-    }
-    
-    // MARK: - Private
-    
-    private func updateViews() {
-        guard isViewLoaded else { return }
-        
-        switch gameState {
-        case let .active(player):
-            statusLabel.text = "Player \(player.stringValue)'s turn"
-        case .cat:
-            statusLabel.text = "Cat's game!"
-        case let .won(player):
-            statusLabel.text = "Player \(player.stringValue) won!"
+            NSLog("Unknown error")
         }
     }
     
@@ -97,7 +74,7 @@ class GameViewController: UIViewController, BoardViewControllerDelegate {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "EmbedBoard" {
-            boardViewController = segue.destination as! BoardViewController
+            boardViewController = segue.destination as? BoardViewController
         }
     }
 }
