5409a817a25a53cf6b77659d8c1f93d9cf98105e
Chad Rutherford <chad_rutherford@outlook.com>
2020-01-22 14:52:31 -0500

    Restructured Project

--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -13,70 +13,88 @@ class GameAITests: XCTestCase {
     
     func testWinCheckingVertical1() {
         var board = GameBoard()
+        
         /*
-        x o -
-        x o -
-        x - -
-        */
+                    x   o   -
+                    x   o   -
+                    x   -   -
+                    */
+        
         try! board.place(mark: .x, on: (0, 0))
         try! board.place(mark: .o, on: (1, 0))
         try! board.place(mark: .x, on: (0, 1))
         try! board.place(mark: .o, on: (1, 1))
         try! board.place(mark: .x, on: (0, 2))
+        
         XCTAssertTrue(game(board: board, isWonBy: .x))
         XCTAssertFalse(game(board: board, isWonBy: .o))
     }
     
     func testWinCheckingVertical2() {
         var board = GameBoard()
+        
         /*
-         x o -
-         x o -
-         - o -
-         */
+                    x   o   -
+                    x   o   -
+                    -   o   -
+                    */
+        
         try! board.place(mark: .o, on: (1, 0))
         try! board.place(mark: .x, on: (0, 0))
         try! board.place(mark: .o, on: (1, 1))
         try! board.place(mark: .x, on: (0, 1))
         try! board.place(mark: .o, on: (1, 2))
+        
         XCTAssertTrue(game(board: board, isWonBy: .o))
         XCTAssertFalse(game(board: board, isWonBy: .x))
     }
     
     func testWinCheckingHorizontal1() {
         var board = GameBoard()
+        
         /*
-         - o -
-         x x x
-         o - -
-         */
+                    -   o   -
+                    x   x   x
+                    o   -   -
+                    */
+        
+        
     }
     
     func testWinCheckingHorizontal2() {
         var board = GameBoard()
+        
         /*
-         x - -
-         - x -
-         o o o
-         */
+                    x   -   -
+                    -   x   -
+                    o   o   o
+                    */
+        
+        
     }
     
     func testWinCheckingDiagonal1() {
         var board = GameBoard()
+        
         /*
-         x - -
-         - x -
-         o o x
-         */
+                    x   -   -
+                    -   x   -
+                    o   o   x
+                    */
+        
+        
     }
     
     func testWinCheckingDiagonal2() {
         var board = GameBoard()
+        
         /*
-         x - o
-         - o -
-         o x -
-         */
+                    x   -   o
+                    -   o   -
+                    o   x   -
+                    */
+        
+        
     }
     
     func testIncompleteGame() {
