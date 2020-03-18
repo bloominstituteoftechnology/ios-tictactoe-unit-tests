927ba465627a1bd5f63065283fef66ca1b7b17f4
elizabeth paige <wingateep@Icloud.com>
2020-03-18 18:02:05 -0400

    filled out GameAITests

--- TicTacToe/Game.swift
+++ TicTacToe/Game.swift
@@ -23,10 +23,29 @@ struct Game {
     }
     
     mutating internal func restart() {
-        
+        board = GameBoard()
+        gameIsOver = false
+        activePlayer = .x
     }
     
     mutating internal func makeMark(at coordinate: Coordinate) throws {
-        
+        guard let activePlayer = activePlayer else { return }
+
+        do {
+            try board.place(mark: activePlayer, on: coordinate)
+            if game(board: board, isWonBy: activePlayer) {
+                gameIsOver = true
+                winningPlayer = activePlayer
+            } else if board.isFull {
+                gameIsOver = true
+                winningPlayer = nil
+            } else {
+                self.activePlayer = activePlayer == .x ? GameBoard.Mark.o : GameBoard.Mark.x
+                gameIsOver = false
+                winningPlayer = nil
+            }
+        } catch {
+            NSLog("Illegal move")
+        }
     }
 }
