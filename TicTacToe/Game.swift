e963c697ef99e9f036d388b42606a962a2ee5153
Enrique Gongora <EGTech24@gmail.com>
2020-03-18 16:29:14 -0700

    Finished Game.Swift file. Some tests  failing.

--- TicTacToe/Game.swift
+++ TicTacToe/Game.swift
@@ -11,16 +11,40 @@ import Foundation
 struct Game {
     
     mutating internal func restart() {
-        
+        board = GameBoard()
+        gameIsOver = false
+        activePlayer = .x
     }
     
-    
     mutating internal func makeMark(at coordinate: Coordinate) throws {
-        
+        guard let activePlayer = activePlayer else { return }
+
+        do {
+            try board.place(mark: activePlayer, on: coordinate)
+            if game(board: board, isWonBy: activePlayer) {
+                gameIsOver = true
+                winningPlayer = activePlayer
+            } else if board.isFull {
+                gameIsOver = true
+                winningPlayer = nil
+            } else {
+                self.activePlayer = activePlayer == .x ? GameBoard.Mark.o : GameBoard.Mark.x
+                gameIsOver = false
+                winningPlayer = nil
+            }
+        } catch {
+            NSLog("Illegal move")
+        }
     }
     
     private(set) var board: GameBoard
     internal var activePlayer: GameBoard.Mark?
     internal var gameIsOver: Bool
     internal var winningPlayer: GameBoard.Mark?
+    
+    init() {
+        board = GameBoard()
+        gameIsOver = false
+        activePlayer = .x
+    }
 }
