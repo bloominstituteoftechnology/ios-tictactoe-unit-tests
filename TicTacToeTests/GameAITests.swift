b9b1a54d73920f2f234c123df0eea0484e9c2bd8
Sal <salmanamer@gmail.com>
2020-04-14 20:25:35 -0700

    Successfully tested draw game - testCatsGame()

--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -108,9 +108,30 @@ class GameAITests: XCTestCase {
         XCTAssertFalse(game(board: board, isWonBy: .x))
     }
     
+    // leave empty slots and no one wins
     func testIncompleteGame() {
+        
     }
-
+    // Draw game
     func testCatsGame() {
+        var board = GameBoard()
+        /*
+        x x o
+        o x x
+        x o o
+        */
+        try! board.place(mark: .x, on: (0,0))
+        try! board.place(mark: .o, on: (2,0))
+        try! board.place(mark: .x, on: (1,0))
+        try! board.place(mark: .o, on: (0,1))
+        try! board.place(mark: .x, on: (1,1))
+        try! board.place(mark: .o, on: (1,2))
+        try! board.place(mark: .x, on: (2,1))
+        try! board.place(mark: .o, on: (2,2))
+        try! board.place(mark: .x, on: (0,2))
+        XCTAssertFalse(game(board: board, isWonBy: .o))
+        XCTAssertFalse(game(board: board, isWonBy: .x))
+//        XCTAssertTrue(board.isFull)
+         
     }
 }
