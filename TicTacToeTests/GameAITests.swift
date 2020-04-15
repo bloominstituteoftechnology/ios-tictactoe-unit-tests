2dad73a85d198a34992d87ddab6dbad215c5b2b8
Sal <salmanamer@gmail.com>
2020-04-14 20:30:34 -0700

    successfully testing func testIncompleteGame()

--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -110,6 +110,19 @@ class GameAITests: XCTestCase {
     
     // leave empty slots and no one wins
     func testIncompleteGame() {
+        var board = GameBoard()
+        /*
+        x x o
+        o x -
+        - x -
+        */
+        try! board.place(mark: .x, on: (0,0))
+        try! board.place(mark: .o, on: (2,0))
+        try! board.place(mark: .x, on: (1,0))
+        try! board.place(mark: .o, on: (0,1))
+        try! board.place(mark: .x, on: (1,2))
+        XCTAssertFalse(game(board: board, isWonBy: .o))
+        XCTAssertFalse(game(board: board, isWonBy: .x))
         
     }
     // Draw game
