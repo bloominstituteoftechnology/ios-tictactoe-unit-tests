afa9fe421bc8f5da07949964d5de81e676bf1d76
Hector L <hectoremil21@hotmail.com>
2020-04-15 19:43:05 -0400

    testPlacingMark fails at checking that values are no longer nil.

--- TicTacToeTests/GameTests.swift
+++ TicTacToeTests/GameTests.swift
@@ -12,11 +12,32 @@ import XCTest
 class GameTests: XCTestCase {
 
     func testCreatingGame() {
-        var game = Game()
+        let game = Game()
         let board = game.board
         
         for x in 0..<3 {
             for y in 0..<3 {
+                XCTAssertNil(board[(x, y)])
+            }
+        }
+    }
+    
+    func testPlacingMark() {
+        var game = Game()
+        let board = game.board
+        
+        // Should be X
+        try! game.makeMark(at: (0, 1))
+        XCTAssertEqual(board[(0, 1)], GameBoard.Mark.x)
+        
+        // Should be Y
+        try! game.makeMark(at: (1,1))
+        XCTAssertEqual(board[(1, 1)], GameBoard.Mark.o)
+        
+        // Check everything else remains
+        for x in 0..<3 {
+            for y in 0..<3 {
+                if (x == 0 && y == 1) || (x == 1 && y == 1) { continue }
                 XCTAssertNil(board[(x, y)])
             }
         }
