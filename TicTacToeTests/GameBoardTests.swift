aa63c878c2627e46ce7c466600f8ba2a92b899c9
Mark Gerrior <github@gerrior.com>
2020-04-15 09:33:38 -0700

    Added gameboard tests to target

---
+++ TicTacToeTests/GameBoardTests.swift
@@ -0,0 +1,18 @@
+//
+//  GameBoardTests.swift
+//  TicTacToeTests
+//
+//  Created by Mark Gerrior on 4/15/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class GameBoardTests: XCTestCase {
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
