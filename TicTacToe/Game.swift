47e444a3a8197ade137cb2f70684638858c981ad
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2020-04-14 22:41:24 -0400

    Basic outline for Game.swift

---
+++ TicTacToe/Game.swift
@@ -0,0 +1,25 @@
+//
+//  Game.swift
+//  TicTacToe
+//
+//  Created by Christy Hicks on 4/14/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    mutating internal func restart() {
+        
+    }
+    
+    mutating internal func makeMark(at coordinate: Coordinate) throws {
+        
+    }
+    
+    private(set) var board: GameBoard
+    
+    internal var activePlayer: GameBoard.Mark?
+    internal var gameIsOver: Bool
+    internal var winningPlayer: GameBoard.Mark?
+}
