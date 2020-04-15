2fadd4b0ff9449753e94da92dcdbf4fc9051320f
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2020-04-14 23:28:56 -0400

    Starting to fill out Game.swift with active code.

--- TicTacToe/Game.swift
+++ TicTacToe/Game.swift
@@ -9,17 +9,47 @@
 import Foundation
 
 struct Game {
-    mutating internal func restart() {
-        
+    
+    enum GameState {
+        case active(GameBoard.Mark) // Active player
+        case cat
+        case won(GameBoard.Mark) // Winning player
+        // case lost(GameBoard.Mark) // Player loses, use if implementing AI opponent.
     }
     
-    mutating internal func makeMark(at coordinate: Coordinate) throws {
-        
-    }
-    
+    // Board and gameState can only be created/changed in this file, but can be read from other files.
     private(set) var board: GameBoard
-    
+    private(set) var gameState = GameState.active(.x)
+
     internal var activePlayer: GameBoard.Mark?
     internal var gameIsOver: Bool
     internal var winningPlayer: GameBoard.Mark?
+
+    mutating internal func restart() {
+        board = GameBoard()
+        gameState = .active(.x)
+    }
+    
+    mutating internal func makeMark(at coordinate: Coordinate) throws {
+        guard case let GameState.active(player) = gameState else {
+            NSLog("Game is over")
+            return
+        }
+        
+        do {
+            try board.place(mark: player, on: coordinate)
+            if game(board: board, isWonBy: player) {
+                gameState = .won(player)
+            } else if board.isFull {
+                gameState = .cat
+            } else {
+                let newPlayer = player == .x ? GameBoard.Mark.o : GameBoard.Mark.x
+                gameState = .active(newPlayer)
+            }
+        } catch {
+            NSLog("Illegal move")
+        }
+    }
+    
+    
 }
