9f642784c19b277e40264b95f3aa3bad17eea8c7
Hector L <hectoremil21@hotmail.com>
2020-04-15 16:50:47 -0400

    testIncompleteGame()

--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -180,8 +180,41 @@ class GameAITests: XCTestCase {
     }
     
     func testIncompleteGame() {
+        var board = GameBoard()
+        /*
+        o o -
+        x o o
+        o x x
+        */
+        
+        try! board.place(mark: .o, on: (1, 1))
+        try! board.place(mark: .x, on: (1, 2))
+        try! board.place(mark: .o, on: (2, 1))
+        try! board.place(mark: .x, on: (0, 1))
+        try! board.place(mark: .o, on: (0, 0))
+        try! board.place(mark: .x, on: (2, 2))
+        try! board.place(mark: .o, on: (1, 0))
+        
+        // Is the game not full?
+        XCTAssertFalse(board.isFull)
+        // Did x not win?
+        XCTAssertFalse(game(board: board, isWonBy: .x))
+        // Did o not win?
+        XCTAssertFalse(game(board: board, isWonBy: .o))
+        // Is it not a cat game?
+        
     }
 
     func testCatsGame() {
+        let board = GameBoard()
+        /*
+        o o x
+        x o o
+        o x x
+        */
+        
+//        try! board.place(mark: .o, on: (1, 1))
+//        try! board.place(mark: .o, on: (1, 1))
+        
     }
 }
