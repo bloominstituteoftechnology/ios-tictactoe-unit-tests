e97ac9e12e42aa090f43a986d32d606c5574fc8e
Shawn Gee <shawngee@gmail.com>
2020-04-15 16:21:10 -0400

    Created new Game struct to house game logic

---
+++ TicTacToe/Game.swift
@@ -0,0 +1,13 @@
+//
+//  Game.swift
+//  TicTacToe
+//
+//  Created by Shawn Gee on 4/15/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    
+}
