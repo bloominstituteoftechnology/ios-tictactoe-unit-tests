c435cd66b39780edb99eeaf7241c75575d587182
Sal <salmanamer@gmail.com>
2020-04-14 19:51:32 -0700

    testWinCheckingHorizontalO working

--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -57,7 +57,7 @@ class GameAITests: XCTestCase {
         try! board.place(mark: .x, on: (2,1))
         XCTAssertTrue(game(board: board, isWonBy: .x))
         XCTAssertFalse(game(board: board, isWonBy: .o))
-        //
+        
     }
     
     func testWinCheckingHorizontalO() {
@@ -67,6 +67,13 @@ class GameAITests: XCTestCase {
          - x -
          o o o
          */
+        try! board.place(mark: .o, on: (0,2))
+        try! board.place(mark: .x, on: (1,1))
+        try! board.place(mark: .o, on: (1,2))
+        try! board.place(mark: .x, on: (0,0))
+        try! board.place(mark: .o, on: (2,2))
+        XCTAssertTrue(game(board: board, isWonBy: .o))
+        XCTAssertFalse(game(board: board, isWonBy: .x))
     }
     
     func testWinCheckingDiagonal1() {
