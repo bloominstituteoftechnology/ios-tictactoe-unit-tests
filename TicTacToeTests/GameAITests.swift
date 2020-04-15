0c8e77e24f75f7183c4d9d2261851c547bb4a4d8
Hector L <hectoremil21@hotmail.com>
2020-04-15 17:00:48 -0400

    CatsGametest

--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -209,15 +209,33 @@ class GameAITests: XCTestCase {
     }
 
     func testCatsGame() {
-        let board = GameBoard()
+        var board = GameBoard()
         /*
         o o x
         x o o
         o x x
         */
         
-//        try! board.place(mark: .o, on: (1, 1))
-//        try! board.place(mark: .o, on: (1, 1))
+        try! board.place(mark: .o, on: (1, 1))
+        try! board.place(mark: .x, on: (1, 2))
+        try! board.place(mark: .o, on: (2, 1))
+        try! board.place(mark: .x, on: (0, 1))
+        try! board.place(mark: .o, on: (0, 2))
+        try! board.place(mark: .x, on: (2, 0))
+        try! board.place(mark: .o, on: (0, 0))
+        try! board.place(mark: .x, on: (2, 2))
+        try! board.place(mark: .o, on: (1, 0))
         
+        // Is the game full?
+        XCTAssertTrue(board.isFull)
+        // Did x not win?
+        XCTAssertFalse(game(board: board, isWonBy: .x))
+        // Did o not win?
+        XCTAssertFalse(game(board: board, isWonBy: .o))
+        
+        /*
+         My logic is as follows:
+            If the board is full and no one won, we got a cat's game.
+         */
     }
 }
