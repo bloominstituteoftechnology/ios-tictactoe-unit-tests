1c1167f9dd54e4e2d1cd5fa01c526d9519a90378
Mark Gerrior <github@gerrior.com>
2020-04-15 10:54:31 -0700

    check column 1 in AI test

--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -7,11 +7,36 @@
 //
 
 import XCTest
+@testable import TicTacToe
 
 class GameAITests: XCTestCase {
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testWhenCheckingVertical0() throws {
+        var board = GameBoard()
+        
+        try! board.place(mark: .x, on: (0,0))
+        try! board.place(mark: .o, on: (1,0))
+        
+        try! board.place(mark: .x, on: (0,1))
+        try! board.place(mark: .o, on: (1,1))
+
+        XCTAssertFalse(game(board: board, isWonBy: .x))
+        XCTAssertFalse(game(board: board, isWonBy: .o))
+
+        try! board.place(mark: .x, on: (0,2))
+
+        XCTAssertTrue(game(board: board, isWonBy: .x))
+        XCTAssertFalse(game(board: board, isWonBy: .o))
+    }
+    
+    func testWinCheckingVertical1() {
+        var board = GameBoard()
+        try! board.place(mark: .x, on: (1, 0))
+        try! board.place(mark: .o, on: (2, 0))
+        try! board.place(mark: .x, on: (1, 1))
+        try! board.place(mark: .o, on: (2, 1))
+        try! board.place(mark: .x, on: (1, 2))
+        XCTAssertTrue(game(board: board, isWonBy: .x))
+        XCTAssertFalse(game(board: board, isWonBy: .o))
     }
 }
