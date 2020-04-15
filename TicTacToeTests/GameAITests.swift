6b455b6a57158018a2670406c0c2dc5344132b74
Sal <salmanamer@gmail.com>
2020-04-14 19:59:20 -0700

    successfully tested testWinCheckingDiagonalO

--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -92,13 +92,20 @@ class GameAITests: XCTestCase {
         XCTAssertFalse(game(board: board, isWonBy: .o))
     }
     
-    func testWinCheckingDiagonal2() {
+    func testWinCheckingDiagonalO() {
         var board = GameBoard()
         /*
          x - o
          - o -
          o x -
          */
+        try! board.place(mark: .o, on: (2,0))
+        try! board.place(mark: .x, on: (0,0))
+        try! board.place(mark: .o, on: (1,1))
+        try! board.place(mark: .x, on: (1,2))
+        try! board.place(mark: .o, on: (0,2))
+        XCTAssertTrue(game(board: board, isWonBy: .o))
+        XCTAssertFalse(game(board: board, isWonBy: .x))
     }
     
     func testIncompleteGame() {
