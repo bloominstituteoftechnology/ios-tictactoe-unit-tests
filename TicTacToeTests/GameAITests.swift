7c849cc8dc82893985a45692961ac398025f1b86
Shawn Gee <shawngee@gmail.com>
2020-04-15 16:26:52 -0400

    Fixed incomplete game test

--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -116,9 +116,12 @@ class GameAITests: XCTestCase {
         */
         try! board.place(mark: .o, on: (2, 0))
         try! board.place(mark: .x, on: (0, 0))
-        try! board.place(mark: .o, on: (1, 1))
-        try! board.place(mark: .x, on: (1, 2))
+        try! board.place(mark: .o, on: (1, 0))
+        try! board.place(mark: .x, on: (0, 1))
         try! board.place(mark: .o, on: (0, 2))
+        try! board.place(mark: .x, on: (1, 1))
+        try! board.place(mark: .o, on: (2, 2))
+        try! board.place(mark: .x, on: (1, 2))
         XCTAssertFalse(game(board: board, isWonBy: .x))
         XCTAssertFalse(game(board: board, isWonBy: .o))
     }
