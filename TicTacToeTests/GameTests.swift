f7347109c5f588775936a33be48c26eeeeb22220
Shawn Gee <shawngee@gmail.com>
2020-04-15 16:36:00 -0400

    Stubbed out GameTests

---
+++ TicTacToeTests/GameTests.swift
@@ -0,0 +1,35 @@
+//
+//  GameTests.swift
+//  TicTacToeTests
+//
+//  Created by Shawn Gee on 4/15/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+@testable import TicTacToe
+
+class GameTests: XCTestCase {
+
+    func testActivePlayer() {
+        
+    }
+    
+    func testGameIsOver() {
+        
+    }
+    
+    func testWinningPlayer() {
+        
+    }
+    
+    func testRestart() {
+        
+    }
+    
+    func testMakeMark() {
+        
+    }
+    
+
+}
