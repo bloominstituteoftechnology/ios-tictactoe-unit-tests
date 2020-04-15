663685b4becc0624e8128154239d2c84668a7d8e
Wyatt Harrell <wyattaharrell@gmail.com>
2020-04-15 18:26:14 -0400

    MVP

--- TicTacToe/GameViewController.swift
+++ TicTacToe/GameViewController.swift
@@ -10,48 +10,25 @@ import UIKit
 
 class GameViewController: UIViewController, BoardViewControllerDelegate {
     
-    private var gameState = GameState.active(.x) {
+    private var game = Game() {
         didSet {
-            updateViews()
+            boardViewController.board = game.board
         }
-    }
-    
-    private var board = GameBoard() {
-        didSet {
-            boardViewController.board = board
-        }
-    }
-    
-    private enum GameState {
-        case active(GameBoard.Mark) // Active player
-        case cat
-        case won(GameBoard.Mark) // Winning player
     }
     
     @IBAction func restartGame(_ sender: Any) {
-        board = GameBoard()
-        gameState = .active(.x)
+        game.restart()
     }
     
     // MARK: - BoardViewControllerDelegate
     
     func boardViewController(_ boardViewController: BoardViewController, markWasMadeAt coordinate: Coordinate) {
-        guard case let GameState.active(player) = gameState else {
-            NSLog("Game is over")
-            return
-        }
         
         do {
-            try board.place(mark: player, on: coordinate)
-            if game(board: board, isWonBy: player) {
-                gameState = .won(player)
-            } else if board.isFull {
-                gameState = .cat
-            } else {
-                let newPlayer = player == .x ? GameBoard.Mark.o : GameBoard.Mark.x
-                gameState = .active(newPlayer)
-            }
+            try game.makeMark(at: coordinate)
+            updateViews()
         } catch {
+            statusLabel.text = "Illegal move"
             NSLog("Illegal move")
         }
     }
@@ -61,14 +38,23 @@ class GameViewController: UIViewController, BoardViewControllerDelegate {
     private func updateViews() {
         guard isViewLoaded else { return }
         
-        switch gameState {
-        case let .active(player):
-            statusLabel.text = "Player \(player.stringValue)'s turn"
-        case .cat:
-            statusLabel.text = "Cat's game!"
-        case let .won(player):
-            statusLabel.text = "Player \(player.stringValue) won!"
+        switch game.activePlayer {
+        case .x:
+            statusLabel.text = "Player X's turn"
+        case .o:
+            statusLabel.text = "Player O's turn"
+        case nil:
+            if let winner = game.winningPlayer {
+                statusLabel.text = "Player \(winner.stringValue.capitalized) won!"
+            } else {
+                statusLabel.text = "Cat's game!"
+            }
         }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateViews()
     }
     
     // MARK: - Navigation
@@ -84,7 +70,7 @@ class GameViewController: UIViewController, BoardViewControllerDelegate {
             boardViewController?.delegate = nil
         }
         didSet {
-            boardViewController?.board = board
+            boardViewController?.board = game.board
             boardViewController?.delegate = self
         }
     }
