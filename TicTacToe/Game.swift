365423fd0ea5da46637c52c63d4409e674e78a08
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2020-04-15 20:39:02 -0400

    Bug fix with MakeMark throwing an error

--- TicTacToe/Game.swift
+++ TicTacToe/Game.swift
@@ -14,7 +14,7 @@ struct Game {
     internal var activePlayer: GameBoard.Mark? = .x // Set default active player to .x
     internal var gameIsOver: Bool = false // Set default to false so game can proceed.
     internal var winningPlayer: GameBoard.Mark? = nil // Set default to nil. Will change if a player wins.
-
+    
     // MARK: - Methods
     mutating internal func restart() { // restarts game after it has completed.
         board = GameBoard()
@@ -25,22 +25,19 @@ struct Game {
     
     mutating internal func makeMark(at coordinate: Coordinate) throws { // Adds a mark, if possible, or ends the game.
         guard let player = activePlayer else { return }
-        do {
-            try board.place(mark: player, on: coordinate)
-            if game(board: board, isWonBy: player) {
-                winningPlayer = player
-                gameIsOver = true
-                activePlayer = nil
-            } else if board.isFull {
-                winningPlayer = nil
-                gameIsOver = true
-                activePlayer = nil
-            } else {
-                let newPlayer = player == .x ? GameBoard.Mark.o : GameBoard.Mark.x
-                activePlayer = newPlayer
-            }
-        } catch {
-            NSLog("Illegal move")
+        
+        try board.place(mark: player, on: coordinate)
+        if game(board: board, isWonBy: player) {
+            winningPlayer = player
+            gameIsOver = true
+            activePlayer = nil
+        } else if board.isFull {
+            winningPlayer = nil
+            gameIsOver = true
+            activePlayer = nil
+        } else {
+            let newPlayer = player == .x ? GameBoard.Mark.o : GameBoard.Mark.x
+            activePlayer = newPlayer
         }
     }
 }
--- TicTacToeTests/GameTests.swift
+++ TicTacToeTests/GameTests.swift
@@ -10,196 +10,56 @@ import XCTest
 @testable import TicTacToe
 
 class GameTests: XCTestCase {
-
-   var board = GameBoard()
+    func testTogglePlayer() {
+        var game = Game()
         
-        func newGame() {
-            var board = GameBoard()
-        }
-
-        func markO(x: Int, y: Int) {
-            
-        }
-        
-        func markX(x: Int, y: Int) {
-          
-        }
-        
-        func xWins() {
-            XCTAssertTrue(true) // Insert X Wins here
-            XCTAssertFalse(false) // Insert O Wins here
-        }
-        
-        func oWins() {
-            XCTAssertTrue(true) // Insert O Wins here
-            XCTAssertFalse(false) // Insert X Wins here
-        }
-        
-        func noWin() {
-            XCTAssertFalse(false) // Insert x wins here
-            XCTAssertFalse(false)   // Insert o wins here
-        }
-        
-        func testWinCheckingVertical1() {
-            /*
-            x o -
-            x o -
-            x - -
-            */
-            newGame()
-            markX(x: 0, y: 0)
-            markO(x: 1, y: 0)
-            markX(x: 0, y: 1)
-            markO(x: 1, y: 1)
-            markX(x: 0, y: 2)
-
-            xWins()
-        }
-        
-        func testWinCheckingVertical2() {
-            /*
-             x o -
-             x o -
-             - o -
-             */
-            newGame()
-            markO(x: 1, y: 0)
-            markX(x: 0, y: 0)
-            markO(x: 1, y: 1)
-            markX(x: 0, y: 1)
-            markO(x: 1, y: 2)
-
-            oWins()
-        }
-        
-        func testWinCheckingVertical3() {
-            /*
-            - o x
-            - o x
-            - - x
-            */
-            newGame()
-            markX(x: 2, y: 0)
-            markO(x: 1, y: 0)
-            markX(x: 2, y: 1)
-            markO(x: 1, y: 1)
-            markX(x: 2, y: 2)
-
-            xWins()
-        }
-        
-        func testWinCheckingHorizontal1() {
-            /*
-             o o o
-             x x -
-             - - -
-             */
-            newGame()
-            markO(x: 0, y: 0)
-            markX(x: 0, y: 1)
-            markO(x: 1, y: 0)
-            markX(x: 1, y: 1)
-            markO(x: 2, y: 0)
-            
-            oWins()
-        }
-        
-        func testWinCheckingHorizontal2() {
-            /*
-             - o -
-             x x x
-             o - -
-             */
-            newGame()
-            markX(x: 0, y: 1)
-            markO(x: 1, y: 0)
-            markX(x: 1, y: 1)
-            markO(x: 0, y: 2)
-            markX(x: 2, y: 1)
-
-            xWins()
-        }
-        
-        func testWinCheckingHorizontal3() {
-            /*
-             x - -
-             - x -
-             o o o
-             */
-            newGame()
-            markO(x: 0, y: 2)
-            markX(x: 0, y: 0)
-            markO(x: 1, y: 2)
-            markX(x: 1, y: 1)
-            markO(x: 2, y: 2)
-            
-            oWins()
-        }
-        
-        func testWinCheckingDiagonal1() {
-            /*
-             x - -
-             - x -
-             o o x
-             */
-            newGame()
-            markX(x: 0, y: 0)
-            markO(x: 0, y: 2)
-            markX(x: 1, y: 1)
-            markO(x: 1, y: 2)
-            markX(x: 2, y: 2)
-            
-            xWins()
-        }
-        
-        func testWinCheckingDiagonal2() {
-            /*
-             x - o
-             - o -
-             o x -
-             */
-            newGame()
-            markO(x: 2, y: 0)
-            markX(x: 0, y: 0)
-            markO(x: 1, y: 1)
-            markX(x: 1, y: 2)
-            markO(x: 0, y: 2)
-            
-            oWins()
-        }
-        
-        func testIncompleteGame() {
-            /*
-             x - o
-             - x -
-             - o -
-             */
-            newGame()
-            markX(x: 0, y: 0)
-            markO(x: 2, y: 0)
-            markX(x: 1, y: 1)
-            markO(x: 1, y: 2)
-            noWin()
-        }
-
-        func testCatsGame() {
-            /*
-             x o x
-             o x o
-             o x o
-             */
-            newGame()
-            markO(x: 0, y: 2)
-            markX(x: 1, y: 1)
-            markO(x: 0, y: 1)
-            markX(x: 0, y: 0)
-            markO(x: 2, y: 2)
-            markX(x: 1, y: 2)
-            markO(x: 1, y: 0)
-            markX(x: 2, y: 0)
-            markO(x: 2, y: 1)
-            noWin()
-        }
+        XCTAssertEqual(game.activePlayer, .x)
+        try! game.makeMark(at: (1, 1))
+        XCTAssertEqual(game.activePlayer, .o)
+        try! game.makeMark(at: (0, 0))
+        XCTAssertEqual(game.activePlayer, .x)
+        try! game.makeMark(at: (1, 2))
+        XCTAssertFalse(game.activePlayer == .x)
     }
-
+    
+    func testMakeMarkError() {
+        var game = Game()
+        
+        try! game.makeMark(at: (1, 1))
+        XCTAssertThrowsError(try game.makeMark(at: (1, 1)))
+    }
+    
+    func testGameIsNotOver() {
+        var game = Game()
+        
+        XCTAssertFalse(game.gameIsOver)
+        try! game.makeMark(at: (2, 0))
+        try! game.makeMark(at: (1, 0))
+        XCTAssertFalse(game.gameIsOver)
+    }
+    
+    func testRestart() {
+        /*
+         x o x
+         o o x
+         o x x
+         */
+        var game = Game()
+        
+        try! game.makeMark(at: (0, 0))
+        try! game.makeMark(at: (0 ,2))
+        try! game.makeMark(at: (2, 0))
+        try! game.makeMark(at: (1, 0))
+        try! game.makeMark(at: (2, 1))
+        try! game.makeMark(at: (1, 1))
+        try! game.makeMark(at: (1, 2))
+        try! game.makeMark(at: (0, 1))
+        try! game.makeMark(at: (2, 2))
+        
+        game.restart()
+        XCTAssertNil(game.winningPlayer)
+        XCTAssertFalse(game.gameIsOver)
+        XCTAssertNoThrow(try game.makeMark(at: (0, 0)))
+    }
+}
 
