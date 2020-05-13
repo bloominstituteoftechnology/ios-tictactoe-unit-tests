883924be7ba38a68fe279b70f7b63dd6258b5a26
Chris Dobek <dobek.christopher@gmail.com>
2020-05-13 15:47:30 -0400

    Restart function complete

--- TicTacToe/Game.swift
+++ TicTacToe/Game.swift
@@ -8,17 +8,22 @@
 
 import Foundation
 struct Game {
+    
+    // MARK: - Properties
+    private(set) var board: GameBoard
+    internal var activePlayer: GameBoard.Mark?
+    internal var gameIsOver: Bool
+    internal var winningPlayer: GameBoard.Mark?
 
+    //MARK: - Functions
     mutating internal func restart() {
-        
+        board = GameBoard()
+        gameIsOver = false
+        winningPlayer = nil
+        activePlayer = .x
     }
     mutating internal func makeMark(at coordinate: Coordinate) throws {
         
     }
 
-  private(set) var board: GameBoard
-
-  internal var activePlayer: GameBoard.Mark?
-  internal var gameIsOver: Bool
-  internal var winningPlayer: GameBoard.Mark?
 }
