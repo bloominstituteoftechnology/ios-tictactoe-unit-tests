844df29fac9b53a95b20ae305154e6498b0faeab
Hunter Oppel <hunter.oppel@gmail.com>
2020-05-13 15:27:54 -0400

    Added test for vertical 3 win

--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -43,6 +43,22 @@ class GameAITests: XCTestCase {
         XCTAssertFalse(game(board: board, isWonBy: .x))
     }
     
+    func testWinCheckingVertical3() throws {
+        var board = GameBoard()
+        /*
+         - - o
+         x x o
+         - - o
+         */
+        try board.place(mark: .o, on: (2, 0))
+        try board.place(mark: .x, on: (0, 1))
+        try board.place(mark: .o, on: (2, 1))
+        try board.place(mark: .x, on: (1, 1))
+        try board.place(mark: .o, on: (2, 2))
+        XCTAssertTrue(game(board: board, isWonBy: .o))
+        XCTAssertFalse(game(board: board, isWonBy: .x))
+    }
+    
     func testWinCheckingHorizontal1() throws {
         var board = GameBoard()
         /*
