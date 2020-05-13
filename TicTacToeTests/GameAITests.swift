c9eb525e29b7e6b9d080775f9812cf5ccd8c994f
Hunter Oppel <hunter.oppel@gmail.com>
2020-05-13 15:41:39 -0400

    Added test for cats game

--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -156,6 +156,24 @@ class GameAITests: XCTestCase {
         XCTAssertFalse(board.isFull)
     }
 
-    func testCatsGame() {
+    func testCatsGame() throws {
+        var board = GameBoard()
+        /*
+         x o x
+         x x o
+         o x o
+         */
+        try board.place(mark: .x, on: (0, 0))
+        try board.place(mark: .o, on: (1, 0))
+        try board.place(mark: .x, on: (2, 0))
+        try board.place(mark: .o, on: (2, 1))
+        try board.place(mark: .x, on: (0, 1))
+        try board.place(mark: .o, on: (0, 2))
+        try board.place(mark: .x, on: (1, 1))
+        try board.place(mark: .o, on: (2, 2))
+        try board.place(mark: .x, on: (1, 2))
+        XCTAssertFalse(game(board: board, isWonBy: .x))
+        XCTAssertFalse(game(board: board, isWonBy: .o))
+        XCTAssertTrue(board.isFull)
     }
 }
