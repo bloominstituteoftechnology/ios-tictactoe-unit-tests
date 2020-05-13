fb8361fbe71d0b16f2194150bfd2c67614f1c43e
Hunter Oppel <hunter.oppel@gmail.com>
2020-05-13 15:28:34 -0400

    Added test for horizontal 2 win

--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -82,6 +82,13 @@ class GameAITests: XCTestCase {
          - x -
          o o o
          */
+        try board.place(mark: .o, on: (0, 2))
+        try board.place(mark: .x, on: (0, 0))
+        try board.place(mark: .o, on: (1, 2))
+        try board.place(mark: .x, on: (1, 1))
+        try board.place(mark: .o, on: (2, 2))
+        XCTAssertTrue(game(board: board, isWonBy: .o))
+        XCTAssertFalse(game(board: board, isWonBy: .x))
     }
     
     func testWinCheckingDiagonal1() throws {
