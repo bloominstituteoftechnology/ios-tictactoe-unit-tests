1879319f0478081b6a743b36b860f3978519431c
Cameron Collins <cameroncollins19@yahoo.com>
2020-05-13 14:32:00 -0500

    Added 2 more tests cases for diangonal and horizontal

--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -68,6 +68,13 @@ class GameAITests: XCTestCase {
          - x -
          o o o
          */
+        try! board.place(mark: .o, on: (0, 2))
+        try! board.place(mark: .o, on: (1, 2))
+        try! board.place(mark: .o, on: (2, 2))
+        try! board.place(mark: .x, on: (0, 0))
+        try! board.place(mark: .x, on: (1, 1))
+        XCTAssertTrue(game(board: board, isWonBy: .o))
+        XCTAssertFalse(game(board: board, isWonBy: .x))
     }
     
     func testWinCheckingDiagonal1() {
@@ -77,6 +84,13 @@ class GameAITests: XCTestCase {
          - x -
          o o x
          */
+        try! board.place(mark: .o, on: (0, 2))
+        try! board.place(mark: .o, on: (1, 2))
+        try! board.place(mark: .x, on: (2, 2))
+        try! board.place(mark: .x, on: (0, 0))
+        try! board.place(mark: .x, on: (1, 1))
+        XCTAssertFalse(game(board: board, isWonBy: .o))
+        XCTAssertTrue(game(board: board, isWonBy: .x))
     }
     
     func testWinCheckingDiagonal2() {
