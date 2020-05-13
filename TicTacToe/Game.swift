ab67f50d9f705ac3adb4966bd791f45c34114ad4
Hunter Oppel <hunter.oppel@gmail.com>
2020-05-13 18:50:49 -0400

    Attempted implementation for AI game. Plays the entire game without you though.

--- TicTacToe/Game.swift
+++ TicTacToe/Game.swift
@@ -18,7 +18,13 @@ struct Game {
     private(set) var board: GameBoard
     private(set) var gameState = GameState.active
     
-    var activePlayer: GameBoard.Mark? = .x
+    var activePlayer: GameBoard.Mark? = .x {
+        didSet {
+            if activePlayer == GameBoard.Mark.o {
+                self.minimax(board: board, player: .o)
+            }
+        }
+    }
     var gameIsOver = false
     var winningPlayer: GameBoard.Mark?
     
@@ -49,5 +55,60 @@ struct Game {
             let newPlayer = player == .x ? GameBoard.Mark.o : GameBoard.Mark.x
             activePlayer = newPlayer
         }
+    }
+    
+    enum Player: Int {
+        case Human = -1
+        case Blank = 0
+        case Computer = 1
+    }
+    
+    mutating func minimax(board: GameBoard, player: GameBoard.Mark) -> Int {
+        var score = -2
+        var aiMove: Coordinate = (0, 0)
+        
+        for x in 0...2 {
+            for y in 0...2 {
+                if board[(x, y)] == nil {
+                    do {
+                        var boardWithNewMove = board // Copy board to make it mutable
+                        try boardWithNewMove.place(mark: .o, on: (x, y)) // Try the move
+                        let scoreForTheMove = -self.minimax(board: boardWithNewMove, player: player == .x ? .x : .o) // Count negative score for oponnent
+                        if scoreForTheMove > score {
+                            score = scoreForTheMove
+                            aiMove = (x, y)
+                            print("New best move is (\(x), \(y))")
+                        }
+                    } catch {
+                        NSLog("Illegal move (⚠️ this message shouldn't appear)")
+                    }
+                }
+            }
+        }
+        
+        if player == .o {
+            do {
+                try self.makeMark(at: aiMove)
+            } catch {
+                NSLog("AI calculation failed.")
+            }
+        }
+        
+        return score
+//        for i in 0...9 { // For all moves
+//            if let _ = board[()] { // Only possible moves
+//                var boardWithNewMove = board // Copy board to make it mutable
+//                boardWithNewMove[i] = player // Try the move
+//                let scoreForTheMove = -self.minimax(boardWithNewMove, player: self.getOponnentFor(player)) // Count negative score for oponnent
+//                if scoreForTheMove > score {
+//                    score = scoreForTheMove
+//                    move = i
+//                } // Picking move that gives oponnent the worst score
+//            }
+//        }
+//        if move == -1 {
+//            return 0 // No move - it's a draw
+//        }
+//        return score
     }
 }
