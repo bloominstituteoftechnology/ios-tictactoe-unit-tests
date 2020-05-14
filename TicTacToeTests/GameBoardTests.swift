2a9bfc0bf8ced1da669f2c79c6087155d947d0e6
Ezra Black <dev.ezrablack@gmail.com>
2020-05-14 02:43:38 -0500

    Added a unit test making sure marks can be placed properly

--- TicTacToeTests/GameBoardTests.swift
+++ TicTacToeTests/GameBoardTests.swift
@@ -28,11 +28,36 @@ class GameBoardTests: XCTestCase {
         XCTAssertNil(gameboard[(x: 1, y: 2)])
         XCTAssertNil(gameboard[(x: 2, y: 2)])
         
+       
+    }
+    
+    func testPlacingMarks() {
+        var gameboard = GameBoard()
+        
+//        try board.place(mark: .o, on: (0, 0))
+        
+        //1)put a piece on the board and check if it throws
+        XCTAssertNoThrow(try gameboard.place(mark: .o, on: (0, 0)))
+        //2)check if the piece is on the board
+        XCTAssertEqual(gameboard[(0, 0)], .o)
+        
+        XCTAssertNoThrow(try gameboard.place(mark: .x, on: (2, 2)))
+        XCTAssertEqual(gameboard[(2, 2)], .x)
+        
+        //Make sure things don't change if we place a O on the same tile.
+        XCTAssertThrowsError(try gameboard.place(mark: .o, on: (2, 2)))
+        XCTAssertEqual(gameboard[(2, 2)], .x)
+        
         for y in 0...2 {
-            for x in 0...2 {
-                XCTAssertNil(gameboard[(x, y)], " ⚠️⚠️ The entry at that \(x) and \(y) was \(gameboard[(x,y)]!) ⚠️⚠️")
-            }
-        }
+                   for x in 0...2 {
+                       if x == 0 && y == 0 {continue}
+                       if x == 2 && y == 2 {continue}
+                       XCTAssertNil(gameboard[(x, y)], " ⚠️⚠️ The entry at that \(x) and \(y) was \(gameboard[(x,y)]!) ⚠️⚠️")
+                   }
+               }
+               XCTAssertThrowsError(try gameboard.place(mark: .o, on: (2, 2))) { (error) in
+                   XCTAssertEqual(error as? GameBoardError, .invalidSquare)
+               }
     }
     
 }
