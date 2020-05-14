0391b0dd76e48743a7d8614b68dcf9d8c93a960d
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-05-13 19:01:32 -0700

    Diagonal 2 & incomplete game test filled

--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -86,6 +86,15 @@ class GameAITests: XCTestCase {
          - x -
          o o x
          */
+        
+        try board.place(mark: .x, on: (0, 0))
+        try board.place(mark: .x, on: (1, 1))
+        try board.place(mark: .x, on: (2, 2))
+        try board.place(mark: .o, on: (0, 2))
+        try board.place(mark: .o, on: (1, 2))
+        
+        XCTAssertTrue(game(board: board, isWonBy: .x))
+        XCTAssertFalse(game(board: board, isWonBy: .o))
     }
     
     func testWinCheckingDiagonal2() throws {
@@ -95,11 +104,37 @@ class GameAITests: XCTestCase {
          - o -
          o x -
          */
+        
+        try board.place(mark: .x, on: (0, 0))
+        try board.place(mark: .o, on: (0, 2))
+        try board.place(mark: .o, on: (1, 1))
+        try board.place(mark: .x, on: (1, 2))
+        try board.place(mark: .o, on: (2, 1))
+        
+        XCTAssertTrue(game(board: board, isWonBy: .o))
+        XCTAssertFalse(game(board: board, isWonBy: .x))
     }
     
-    func testIncompleteGame() {
+    func testIncompleteGame() throws {
+        var board = GameBoard()
+        
+      
+        try board.place(mark: .x, on: (0, 0))
+        try board.place(mark: .x, on: (1, 0))
+        try board.place(mark: .x, on: (2, 1))
+        try board.place(mark: .x, on: (0, 2))
+        try board.place(mark: .x, on: (1, 2))
+        
+        try board.place(mark: .o, on: (0, 1))
+        try board.place(mark: .o, on: (1, 1))
+        try board.place(mark: .o, on: (2, 0))
+        try board.place(mark: .o, on: (2, 2))
+        
+        XCTAssertFalse(game(board: board, isWonBy: .o))
+        XCTAssertFalse(game(board: board, isWonBy: .x))
+        
     }
 
-    func testCatsGame() {
+    func testCatsGame() throws {
     }
 }
