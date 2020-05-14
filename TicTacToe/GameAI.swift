1cd30938a70fa575672968b4a24c24f973bfd25a
Ezra Black <dev.ezrablack@gmail.com>
2020-05-14 03:05:28 -0500

    Refactored the code to check all the vertical columns.

--- TicTacToe/GameAI.swift
+++ TicTacToe/GameAI.swift
@@ -11,28 +11,19 @@ import Foundation
 func game(board: GameBoard, isWonBy player: GameBoard.Mark) -> Bool {
     
     //checking vertical column 0
-    var numberOfMarks = 0
     
-    for y in 0...2 {
-        if board[(0,y)] == player {
-            numberOfMarks += 1
+    for x in 0...2 {
+        var numberOfMarks = 0
+        
+        for y in 0...2 {
+            if board[(x,y)] == player {
+                numberOfMarks += 1
+            }
         }
-    }
-    
-    if numberOfMarks == 3 {
-        return true
-    }
-    
-    numberOfMarks = 0
-    
-    for y in 0...2 {
-        if board[(1,y)] == player {
-            numberOfMarks += 1
-        }
+        
         if numberOfMarks == 3 {
             return true
         }
     }
-    
     return false
 }
