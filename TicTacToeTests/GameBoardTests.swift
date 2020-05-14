4649a74b543886f892d81de88599eedec0a7fd0c
Ezra Black <dev.ezrablack@gmail.com>
2020-05-14 02:00:06 -0500

    Added tests cleaned up files for use.

---
+++ TicTacToeTests/GameBoardTests.swift
@@ -0,0 +1,18 @@
+//
+//  GameBoardTests.swift
+//  TicTacToeTests
+//
+//  Created by Ezra Black on 5/14/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class GameBoardTests: XCTestCase {
+    
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
