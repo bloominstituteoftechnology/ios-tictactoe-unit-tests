94db22fc63343019b20d4021506eaaaafbd7e41b
Ezra Black <dev.ezrablack@gmail.com>
2020-05-14 03:13:04 -0500

    Added the functionality to check diagonals.

--- TicTacToe/GameAI.swift
+++ TicTacToe/GameAI.swift
@@ -36,5 +36,31 @@ func game(board: GameBoard, isWonBy player: GameBoard.Mark) -> Bool {
                return true
            }
        }
+    
+    let leftToRight: [Coordinate] = [(0,0),(1,1),(2,2)]
+    var numberOfMarks = 0
+              
+              for coordinate in leftToRight {
+                  if board[coordinate] == player {
+                      numberOfMarks += 1
+                  }
+              }
+              
+              if numberOfMarks == 3 {
+                  return true
+              }
+    
+    let rightToLeft: [Coordinate] = [(2,0),(1,1),(0,2)]
+      numberOfMarks = 0
+                
+                for coordinate in rightToLeft {
+                    if board[coordinate] == player {
+                        numberOfMarks += 1
+                    }
+                }
+                
+                if numberOfMarks == 3 {
+                    return true
+                }
     return false
 }
