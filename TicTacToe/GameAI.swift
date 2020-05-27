f8656cba70053a8b6bea3ab7cd0600701fd2e5f4
Breena <breena_greek@live.com>
2020-05-26 22:50:59 -0400

    Updated to test all columns

--- TicTacToe/GameAI.swift
+++ TicTacToe/GameAI.swift
@@ -10,30 +10,19 @@ import Foundation
 
 func game(board: GameBoard, isWonBy player: GameBoard.Mark) -> Bool {
     
-    // checking vertical column 0
-    var numberofMarks = 0
-    
-    for y in 0...2 {
-        if board[(0, y)] == player {
-            numberofMarks += 1
+    // checking vertical columns
+    for x in 0...2 {
+        var numberofMarks = 0
+        
+        for y in 0...2 {
+            if board[(x, y)] == player {
+                numberofMarks += 1
+            }
         }
-    }
     
     if numberofMarks == 3 {
         return true
-    }
-    
-    // checking vertical column 1
-    numberofMarks = 0
-    
-    for y in 0...2 {
-        if board[(1, y)] == player {
-            numberofMarks += 1
         }
-    }
-    
-    if numberofMarks == 3 {
-        return true
     }
     
     return false
