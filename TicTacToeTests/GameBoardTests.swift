a428661c2dfda3ad4d6780e3752ba528052c1b9e
Breena <breena_greek@live.com>
2020-05-26 22:15:48 -0400

    Turned on Code Coverage, unit test making sure marks can be placed properly

--- TicTacToeTests/GameBoardTests.swift
+++ TicTacToeTests/GameBoardTests.swift
@@ -20,10 +20,12 @@ class GameBoardTests: XCTestCase {
         XCTAssertNil(board[(x: 1, y: 0)])
         XCTAssertNil(board[(x: 2, y: 0)])
         
+        // Check second row
         XCTAssertNil(board[(x: 0, y: 1)])
         XCTAssertNil(board[(x: 1, y: 1)])
         XCTAssertNil(board[(x: 2, y: 1)])
         
+        // Check third row
         XCTAssertNil(board[(x: 0, y: 2)])
         XCTAssertNil(board[(x: 1, y: 2)])
         XCTAssertNil(board[(x: 2, y: 2)])
@@ -32,6 +34,36 @@ class GameBoardTests: XCTestCase {
             for x in 0...2 {
                 XCTAssertNil(board[(x, y)], "⚠️ The entry at (\(x), \(y)) was \(board[(x, y)]!) instead of nil: (.")
             }
+        }
+    }
+    
+    func testPlacingMarks() {
+        var board = GameBoard()
+        
+//        try! board.place(mark: .o, on: (0, 0))
+        // Check .o
+        XCTAssertNoThrow(try! board.place(mark: .o, on: (0, 0)))
+        XCTAssertEqual(board[(0, 0)], .o)
+        
+        // Check .x
+        XCTAssertNoThrow(try! board.place(mark: .x, on: (2, 2)))
+        XCTAssertEqual(board[(2, 2)], .x)
+        
+        // Make sure things dont change if we place an .o on the same tile
+        XCTAssertThrowsError(try! board.place(mark: .o, on: (2, 2)))
+        XCTAssertEqual(board[(2, 2)], .x)
+        
+        for y in 0...2 {
+            for x in 0...2 {
+                if x == 0 && y == 0 { continue }
+                if x == 2 && y == 2 { continue }
+                
+                XCTAssertNil(board[(x, y)], "⚠️ The entry at (\(x), \(y)) was \(board[(x, y)]!) intead of nil: (.")
+            }
+        }
+        
+        XCTAssertThrowsError(try board.place(mark: .o, on: (2, 2))) { (error)  in
+            XCTAssertEqual(error as? GameBoardError, .invalidSquare)
         }
     }
 }
