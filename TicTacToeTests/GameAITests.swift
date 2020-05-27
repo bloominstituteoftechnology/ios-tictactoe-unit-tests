e98b4a98d272d9034fdf97afbe0f76e49014e568
Breena <breena_greek@live.com>
2020-05-26 22:33:36 -0400

    added GameAI Tests Folder

---
+++ TicTacToeTests/GameAITests.swift
@@ -0,0 +1,18 @@
+//
+//  GameAITests.swift
+//  TicTacToeTests
+//
+//  Created by Breena Greek on 5/26/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class GameAITests: XCTestCase {
+
+    func testExample() throws {
+
+    }
+
+
+}
