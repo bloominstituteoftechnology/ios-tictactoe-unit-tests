8fe4f2e616cf2321fd407cd3bf9adb77a01b78dc
Thomas Dye <thomasallendye@gmail.com>
2020-05-27 09:23:04 -0400

    added unit tests to running target

---
+++ TicTacToeTests/GameBoardTests.swift
@@ -0,0 +1,17 @@
+//
+//  GameBoardTests.swift
+//  TicTacToeTests
+//
+//  Created by Thomas Dye on 5/27/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class GameBoardTests: XCTestCase {
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
