d94c859fb2ef479d55a1f96194735f7691cdb170
Cora Jacobson <cora-jacobson@lambdastudents.com>
2020-09-23 17:44:55 -0700

    Parts 2 & 3

---
+++ TicTacToe/Game.swift
@@ -0,0 +1,52 @@
+//
+//  Game.swift
+//  TicTacToe
+//
+//  Created by Cora Jacobson on 9/23/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    
+    enum GameState: Equatable {
+        case active(GameBoard.Mark) // Active player
+        case cat
+        case won(GameBoard.Mark) // Winning player
+    }
+    
+    mutating internal func restart() {
+        board = GameBoard()
+        gameState = .active(.x)
+    }
+    
+    mutating internal func makeMark(at coordinate: Coordinate) throws {
+        guard case let GameState.active(player) = gameState else {
+            NSLog("Game is over")
+            return
+        }
+        
+        do {
+            try board.place(mark: player, on: coordinate)
+            if game(board: board, isWonBy: player) {
+                gameState = .won(player)
+            } else if board.isFull {
+                gameState = .cat
+            } else {
+                let newPlayer = player == .x ? GameBoard.Mark.o : GameBoard.Mark.x
+                gameState = .active(newPlayer)
+            }
+        } catch {
+            NSLog("Illegal move")
+        }
+    }
+    
+    private(set) var board: GameBoard
+    
+    internal var activePlayer: GameBoard.Mark?
+    internal var gameIsOver: Bool
+    internal var winningPlayer: GameBoard.Mark?
+    
+    var gameState = GameState.active(.x)
+}
--- TicTacToe/GameViewController.swift
+++ TicTacToe/GameViewController.swift
@@ -10,37 +10,17 @@ import UIKit
 
 class GameViewController: UIViewController, BoardViewControllerDelegate {
     
-    private enum GameState {
-        case active(GameBoard.Mark) // Active player
-        case cat
-        case won(GameBoard.Mark) // Winning player
-    }
-    
     @IBAction func restartGame(_ sender: Any) {
-        board = GameBoard()
-        gameState = .active(.x)
+        currentGame.restart()
     }
     
     // MARK: - BoardViewControllerDelegate
     
     func boardViewController(_ boardViewController: BoardViewController, markWasMadeAt coordinate: Coordinate) {
-        guard case let GameState.active(player) = gameState else {
-            NSLog("Game is over")
-            return
-        }
-        
         do {
-            try board.place(mark: player, on: coordinate)
-            if game(board: board, isWonBy: player) {
-                gameState = .won(player)
-            } else if board.isFull {
-                gameState = .cat
-            } else {
-                let newPlayer = player == .x ? GameBoard.Mark.o : GameBoard.Mark.x
-                gameState = .active(newPlayer)
-            }
+            try currentGame.makeMark(at: coordinate)
         } catch {
-            NSLog("Illegal move")
+            NSLog("Error making mark")
         }
     }
     
@@ -49,7 +29,7 @@ class GameViewController: UIViewController, BoardViewControllerDelegate {
     private func updateViews() {
         guard isViewLoaded else { return }
         
-        switch gameState {
+        switch currentGame.gameState {
         case let .active(player):
             statusLabel.text = "Player \(player.stringValue)'s turn"
         case .cat:
@@ -72,22 +52,18 @@ class GameViewController: UIViewController, BoardViewControllerDelegate {
             boardViewController?.delegate = nil
         }
         didSet {
-            boardViewController?.board = board
+            boardViewController?.board = currentGame.board
             boardViewController?.delegate = self
         }
     }
     
     @IBOutlet weak var statusLabel: UILabel!
     
-    private var gameState = GameState.active(.x) {
+    var currentGame = Game(board: GameBoard(), gameIsOver: false) {
         didSet {
             updateViews()
+            boardViewController.board = currentGame.board
         }
     }
-    
-    private var board = GameBoard() {
-        didSet {
-            boardViewController.board = board
-        }
-    }
+
 }
